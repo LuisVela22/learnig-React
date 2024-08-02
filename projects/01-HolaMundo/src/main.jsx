87b6eea66bd146872d6170fd44822e90b20f0e0f
Luis Velasco <luisvelaj227@gmail.com>
2024-08-01 21:16:21 -0600

    X-button-follow

--- projects/01-HolaMundo/src/main.jsx
+++ projects/01-HolaMundo/src/main.jsx
@@ -1,23 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { App } from './App.jsx'
+import './index.css' 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-
-const createButton = text => {
-  return (<button>
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-    <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-    </svg>{text}
-  </button>
-
-  )
-}
 root.render(
-  <React.Fragment>
-    {createButton("Hola1")}
-    {createButton("Hola2")}
-    {createButton("Hola3")}
-    {createButton("Hola4")}
-  </React.Fragment>
-  
+  <App />
 )
