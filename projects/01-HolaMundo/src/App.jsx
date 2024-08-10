ea6a700617561827a8dcde72fc9c9d7535b1ed61
Luis Velasco <luisvelaj227@gmail.com>
2024-08-10 15:04:25 -0600

    last commit

--- projects/01-HolaMundo/src/App.jsx
+++ projects/01-HolaMundo/src/App.jsx
@@ -1,35 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import { XFollowCard } from './XFollowCard.jsx'
 
 export function App () {
     const formatUserName = (userName) => `@${userName}`
+    const [name, setName] = useState('Lua Vela');
+    console.log('render with name: ', name);
     return (
         <section className='App'>
-            <XFollowCard 
-                formatUserName={formatUserName} 
-                userName="midudev" 
-                name="Miguel Ángel" 
-                isFollowing //Esto es un true, con false no se puede hacer !isFollowing
-            />
-            <XFollowCard 
-                formatUserName={formatUserName} 
-                userName="midudev" 
-                name="elenamrt" 
-                isFollowing={false} 
-            /> 
-            <XFollowCard 
-                formatUserName={formatUserName} 
-                userName="midudev" 
-                name="elenamrt" 
-                isFollowing={true} 
-            /> 
-            <XFollowCard 
-                formatUserName={formatUserName} 
-                userName="midudev" 
-                name="elenamrt" 
-                isFollowing={false} 
-            />
+            <XFollowCard formatUserName={formatUserName} initialIsFollowing /*Esto es un true, con false no se puede hacer !isFollowing*/>  
+                Miguel Angel
+            </XFollowCard>
+
+            <XFollowCard formatUserName={formatUserName} userName={name}  initialIsFollowing={false}>
+                Luis Velasco
+            </XFollowCard> 
+
+            <XFollowCard formatUserName={formatUserName} userName="midudev"  initialIisFollowing={false}>
+                Angel Velasco 
+            </XFollowCard>
+
+            {/* <button onClick={() =>{
+                setName('Luan Vela');
+            }}>Cambiar Nombre</button> */}
         </section>
     )
 }
--- projects/01-HolaMundo/src/XFollowCard.jsx
+++ projects/01-HolaMundo/src/XFollowCard.jsx
@@ -1,20 +1,42 @@
 import React from "react"
+import { useState } from "react";
 
-export function XFollowCard ({formatUserName, userName, name, isFollowing}) {
+export function XFollowCard ({formatUserName, userName =  'undefined', children, initialIsFollowing}) {
     // const addAt = (userName) => `@${userName}`
-    console.log(isFollowing);
+    //console.log('[XFollowCard] render with Username', userName);
+    
+    //RECORDAR QUE LAS PROPOS SON INMUTABLES, ES DECIR, NO PODEMOS ALTERAR SU CONTENIDO, ES MEJOR
+    //CREAR UNA VARIABLE QUE ALMACENE ALGUNA MODIFICACION DEL CONTENIDO, PERO NUNCA MODIFICARLA COMO TAL
+
+
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing);
+    const [hover, setHover] = useState(false);
+
+    const text = isFollowing ? (hover ? "Dejar de seguir" : "Siguiendo") : "Seguir";
+    const buttonClassName = isFollowing ? 'tw-Stopfollow-button' : 'tw-followCard-button';
+    
+    const handleMouseEnter = () => setHover(true);
+    const handleMouseLeave = () => setHover(false);
+
+    const handleClick = () => setIsFollowing(!isFollowing);
     return (
         <article className='tw-followCard' >
             <header className='tw-followCard-header'>
                 <img className='tw-followCard-avatar' src={`https://unavatar.io/x/${userName}`} alt='' />
                 <div className='tw-followCard-info'>
-                    <strong>{name}</strong>
+                    <strong>{children}</strong>
                     <span className='tw-followCard-infoUserName'>{formatUserName(userName)}</span>
                 </div>
             </header>
             
             <aside>
-                <button className='tw-followCard-button'>Seguir</button>
+                <button className={buttonClassName} 
+                        onClick={handleClick}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}        
+                >
+                    <span className="tw-followCard-text">{text}</span>
+                </button>
             </aside>
             <br />
             <br />
